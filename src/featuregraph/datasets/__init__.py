a7bee4df12e0d2a9e12a1c32d31092255a60c474
featuregraph <nazia.habib@featuregraph.ai>
2026-08-05 16:33:30 -0500

    Export ARC-AGI dataset loader

--- src/featuregraph/datasets/__init__.py
+++ src/featuregraph/datasets/__init__.py
@@ -2,12 +2,14 @@
 FeatureGraph example datasets.
 """
 
+from ._arc_agi import arc_agi
 from ._bidmc import bidmc
 from ._cartpole import cartpole
 from ._eastman import eastman
 from ._mountaincar import mountaincar
 
 __all__ = [
+    "arc_agi",
     "bidmc",
     "cartpole",
     "eastman",
