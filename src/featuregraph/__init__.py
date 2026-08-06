133241376c58e70df99ae34d88c5344c4bf60e64
featuregraph <nazia.habib@featuregraph.ai>
2026-08-06 19:42:51 +0000

    feat(spatial): generalize block composition with background operator

--- src/featuregraph/__init__.py
+++ src/featuregraph/__init__.py
@@ -2,7 +2,7 @@
 FeatureGraph public API.
 """
 
-from .behaviors import oscillation, accumulation
+from .behaviors import oscillation, accumulation, spatial
 from . import datasets
 
 from .utils._plot import plot
@@ -12,6 +12,7 @@ __version__ = "0.1.0a1"
 __all__ = [
     "oscillation",
     "accumulation",
+    "spatial",
     "datasets",
     "plot",
 ]
